e789b4bded3f072c2ba00eacb014cbdac5aa6c3d
razibdash <avrorazib@gmail.com>
2025-09-10 12:00:02 +0600

    page title with metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next'
 import React from 'react'
 
+export const metadata:Metadata={
+  title:"contact"
+}
 const ContactPage = () => {
   return (
     <div>ContactPage</div>
