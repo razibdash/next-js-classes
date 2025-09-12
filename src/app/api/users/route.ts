d23bb61eff35fd8730ae82bd04bd5476c6e70547
razibdash <avrorazib@gmail.com>
2025-09-13 02:56:24 +0600

    handling post request

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,18 @@
+import { json } from "node:stream/consumers";
 import { users } from "./data";
 
 export async function GET(){
     return Response.json(users)
+}
+
+export async function POST(request:Request){
+  const newData=await request.json()
+  const newUser={
+    id:users.length+1,
+    ...newData
+  }
+  users.push(newUser);
+
+  return new Response(JSON.stringify(newUser))
+
 }
