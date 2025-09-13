d779a7635981c67d3d52d048891f715b46f72d85
razibdash <avrorazib@gmail.com>
2025-09-14 00:26:05 +0600

    add query parameters and multiple query parameters

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,24 @@
-import { json } from "node:stream/consumers";
-import { users } from "./data";
 
-export async function GET(){
-    return Response.json(users)
+import { users } from "./data";
+import {NextResponse, type NextRequest } from "next/server";
+
+export async function GET(request:NextRequest){
+  const searchParams=request.nextUrl.searchParams;
+  const query=searchParams.get('search')?.toLowerCase() || '';
+  const numberQuery=searchParams.get('phone')?.toLowerCase()||'';
+   // Filter users by name or email
+  const filteredUsers =query? users.filter(
+    (user) =>{
+     const emailAndName= user.name.toLowerCase().includes(query) ||
+      user.email.toLowerCase().includes(query)
+      const number=user.phone?.toLowerCase().includes(numberQuery)
+      return emailAndName && number;
+    }
+
+      
+  ):users;
+
+  return NextResponse.json(filteredUsers, { status: 200 });
 }
 
 export async function POST(request:Request){
