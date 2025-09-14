48c45564acbdfc333918c9fa119f874a50399264
razibdash <avrorazib@gmail.com>
2025-09-14 13:08:30 +0600

    Working with Headers

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET(request:Request){
-    return new Response("server is running");
+
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request:NextRequest){
+    // const requestHeader= new Headers(request.headers)
+    // console.log(requestHeader.get("Authorization"))
+    const headersList = await headers()
+    const Authorization=headersList.get('Authorization')as string;
+    console.log(Authorization)
+     
+    return NextResponse.json(
+    {
+      message: "server is running",
+      success: true, 
+    },
+    {
+      status: 201,
+      headers: {
+        Authorization:Authorization,
+      },
+    }
+  );
 }
