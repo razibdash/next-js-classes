961c5695c500f4cf5d2082973f01dbd8e2b9e230
razibdash <avrorazib@gmail.com>
2025-09-16 01:09:53 +0600

    add generateStaticParams

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@
 import Comment from '@/app/components/Comment'
 import PostListSkeleton from '@/app/components/PostListSkeleton'
 import React, { Suspense } from 'react'
+// export const dynamicParams=false
 type Props={
     params:Promise<{id:string}>
 }
@@ -12,10 +13,21 @@ type Post={
     body:string
 }
 
+
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts=await res.json();
+ 
+  return posts.slice(0,5).map((post:Post) => ({
+    id:post.id.toLocaleString()
+  }))
+}
+
 async function getPost(id:string):Promise<Post>{
    const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
    return res.json()
 }
+
 const PostPage =async ({params}:Props) => {
   const {id}=await params;
   const post=await getPost(id)
